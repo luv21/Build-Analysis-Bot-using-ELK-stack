e773517a3477ff46ee089801649ef66dbb620848
Karthick Arya <karya2@ncsu.edu>
2019-10-20 08:02:22 -0400

    Simulate Jenkins build trigger and most message on slack

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const messages = require('./messages')
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const config = require('./credentials.json');
 
 // Creates express app
 const app = express(); // The port used for Express server
@@ -46,26 +47,24 @@ app.post("/complete", (req, res) => {
     let temp = data.getBuild(req.body.name)
     body = messages.faiureMessage(temp)
   }
-  console.log(req.body.build.status, body)
-
-  // let body = {
+  console.log(req.body.build.status, config.HOOK_URL)
+  // body = {
   //   text: "Botwa to the rescue :rhinoceros:",
   //   attachments: [
   //     {
   //       text: "Let's defeat Thanos!!! :dog: :dog: :dog:"
   //     }
   //   ]
-  // };
-
-  // request.post(
-  //   {
-  //     headers: { "content-type": "application/json" },
-  //     url: req.body.response_url,
-  //     body: JSON.stringify(body)
-  //   },
-  //   (error, response, body) => {
-  //     console.log("response: ", response.statusCode);
-  //     res.json();
-  //   }
-  // );
+  // }
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: config.HOOK_URL,
+      body: JSON.stringify(body)
+    },
+    (error, response, body) => {
+      console.log("response: ", response.statusCode);
+      res.json();
+    }
+  );
 });
--- server/messages.js
+++ server/messages.js
@@ -1,37 +1,23 @@
 module.exports = {
   successMessage: data => {
     message = {
-      blocks: [
-        {
-          type: "section",
-          text: `Build ${data.name} is Success!!! Click here to go to the PR`
-        }
-      ]
+      text: `Build ${data.name} is Success!!! Click here to go to the PR`
     };
     return message;
   },
 
-  faiureMessage: (elastic) => {
+  faiureMessage: elastic => {
     let count = elastic.errors.length;
-    let errors = [
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: `Build ${elastic.name} Failed with ${count} errors :sad: :sad: :sad: Find details below`
-        }
-      }
-    ];
+    let errors = new Array(0);
     elastic.errors.forEach(error => {
       errors.push({
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: `Type: ${error.text}\n In: ${error.file + " " + error.location}`
-        }
+        text: `Type: ${error.text}\n In: ${error.file + " " + error.location}`
       });
     });
-    return {blocks:errors}
+    return {
+      text: `Build ${elastic.name} Failed with ${count} errors :cry: :cry: :cry: Find details below`,
+      attachments: errors
+    };
   }
 };
 //curl -X POST -H 'Content-type: application/json' --data '{"name": "build1","url": "job/asgard/", "build": {"full_url": "http://localhost:8080/job/asgard/18/", "number": 18,"phase": "COMPLETED","status": "SUCCESsS","url": "job/asgard/18/","scm": {"url": "https://github.ncsu.edu/csc510-fall2019/CSC510-26.git","branch": "origin/master", "commit": "b769b2c1e30e628f1261668bc6ea908e84986291"}}}' localhost:3000/complete
