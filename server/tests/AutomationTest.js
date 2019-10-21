2bb648a33a09016c5c35949bf09c416b139c2f4e
Jayesh <jayesh5397@gmail.com>
2019-10-21 02:54:02 -0400

    page

--- server/tests/AutomationTest.js
+++ server/tests/AutomationTest.js
@@ -4,11 +4,16 @@ const puppeteer = require('puppeteer');
 
 (async () => {
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless:false});
 
     const page = await browser.newPage();
 
     await page.goto("https://slack.com/signin");
 
+    //await page.screenshot({path: 'example.png'});
 
-});
+
+    await browser.close();
+
+
+})();
