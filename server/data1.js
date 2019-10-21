333f28f2d15b0835b85cbbe3d97063993e32098c
Pavan Kumar Madduri <vpmaddur@ncsu.edu>
2019-10-21 05:19:57 -0400

    persist mock interception

--- server/data1.js
+++ server/data1.js
@@ -4,6 +4,7 @@ const data = require("./mock_data/elasticMock.json")
 urlRoot = "https://35.208.168.86:9200"
 
 var elasticsearchmock = nock(urlRoot)
+    .persist()
 	.get("/jenkins/_doc/0?_source=false&pretty")
 	.reply(200, JSON.stringify(data.build1))
 
