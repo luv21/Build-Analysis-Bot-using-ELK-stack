ccfda3b42a1fcb8206cf1bc78b07acc702370218
Jayesh <jayesh5397@gmail.com>
2019-10-22 14:31:27 -0400

    api

--- server/tests/credentials.js
+++ server/tests/credentials.js
@@ -3,5 +3,11 @@ module.exports = {
     workspace: 'se-botwa.slack.com',
     username: 'sjbondu@ncsu.edu',
     password: 'Itachi@05',
-    command:  '\bot-assemble'
+    command:  '\bot-assemble',
+      "SLACK_CLIENT_ID": "801964928469.801959507712",
+      "SLACK_CLIENT_SECRET":"f34fe503aa2ee49cf28f0e2af50f3a59",
+      "SLACK_VERIFICATION_TOKEN":"KCL1KboTfNRmtvyBydXhLKH1",
+      "SLACK_AUTH_TOKEN":"f64d51fa495bc76bfb7f1a4951bc0651",
+      "HOOK_URL": "https://hooks.slack.com/services/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"
+  
   }
