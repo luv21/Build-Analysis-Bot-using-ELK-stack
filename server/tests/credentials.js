72dec54fd3ccf7ffa31b38b95174f266aa4393aa
iamluv <lkhuran@ncsu.edu>
2019-10-22 19:22:13 -0400

    test3.js

--- server/tests/credentials.js
+++ server/tests/credentials.js
@@ -3,5 +3,8 @@ module.exports = {
     workspace: 'se-botwa.slack.com',
     username: 'sjbondu@ncsu.edu',
     password: 'Itachi@05',
-    command:  '\bot-assemble'
+    command: '/bot-assemble',
+    build_fail: 'build1',
+    build_pass: 'build2',
+    url: 'https://slack.com/signin'
   }
