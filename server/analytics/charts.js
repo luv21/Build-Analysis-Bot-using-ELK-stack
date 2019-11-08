437dfe947dbd775515cb90d66a1308e0884da314
Karthick Arya <karya2@ncsu.edu>
2019-11-08 05:43:52 -0500

    Post images onto slack

--- server/analytics/charts.js
+++ server/analytics/charts.js
@@ -1,56 +1,82 @@
-const { CanvasRenderService } = require('chartjs-node-canvas');
-const fs = require('fs');
+const { CanvasRenderService } = require("chartjs-node-canvas");
+const fs = require("fs");
+var Slack = require("node-slack-upload");
+const config = require("../credentials.json");
 
-async function generateChart(){
-    const width = 400; //px
-    const height = 400; //px
-    const configuration = {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        callback: (value) => '$' + value
-                    }
-                }]
-            }
+async function generateChart() {
+  const width = 400; //px
+  const height = 400; //px
+  const configuration = {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)"
+          ],
+          borderColor: [
+            "rgba(255,99,132,1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)"
+          ],
+          borderWidth: 1
         }
-    };
-    const canvasRenderService = new CanvasRenderService(width, height, (ChartJS) => { });
-    const image = await canvasRenderService.renderToBuffer(configuration);
-    fs.writeFile('image.png', image, (err) => {
-        // throws an error, you could also catch it here
-        if (err) throw err;
-    
-        // success case, the file was saved
-        console.log('Lyric saved!');
-    });
-    
+      ]
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+              callback: value => "$" + value
+            }
+          }
+        ]
+      }
+    }
+  };
+  const canvasRenderService = new CanvasRenderService(
+    width,
+    height,
+    ChartJS => {}
+  );
+  const image = await canvasRenderService.renderToBuffer(configuration);
+  fs.writeFile("image.png", image, err => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+    var slack = new Slack(config.BOT_OAUTH_ACCESS_TOKEN);
+    slack.uploadFile(
+      {
+        file: fs.createReadStream("image.png"),
+        filetype: "auto",
+        title: "README",
+        initialComment: "my comment",
+        channels: config.SLACK_CHANNEL
+      },
+      function(err, data) {
+        if (err) {
+          console.error(err);
+        } else {
+        }
+      }
+    );
+    // success case, the file was saved
+    console.log("Image saved");
+  });
 }
 
-generateChart()
+// generateChart();
+
+exports.generateChart = generateChart;
